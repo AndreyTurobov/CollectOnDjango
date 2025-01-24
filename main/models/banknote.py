c3de4cea089ece64b79393c520c4592ec69652b5
andrei_turobov <forgunn@yandex.ru>
2025-01-25 00:30:38 +0500

    image thumbnails have been added

--- main/models/banknote.py
+++ main/models/banknote.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import ResizeToFill
 from .base import CollectorsItem
 
 
@@ -6,8 +8,32 @@ class Banknote(CollectorsItem):
     signature = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     serial_number = models.CharField(max_length=50)
-    averse_image = models.ImageField(upload_to='media/notes_images/', default='default_pic/note-01.jpg')
-    reverse_image = models.ImageField(upload_to='media/notes_images/', default='default_pic/note-02.jpg')
+    averse_image = ProcessedImageField(
+        upload_to='media/notes_images/',
+        default='default_pic/note-01.jpg',
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 90}
+    )
+    reverse_image = ProcessedImageField(
+        upload_to='media/notes_images/',
+        default='default_pic/note-02.jpg',
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 90}
+    )
+    averse_thumbnail = ImageSpecField(
+        source='averse_image',
+        processors=[ResizeToFill(180, 85)],
+        format='JPEG',
+        options={'quality': 80}
+    )
+    reverse_thumbnail = ImageSpecField(
+        source='reverse_image',
+        processors=[ResizeToFill(180, 85)],
+        format='JPEG',
+        options={'quality': 80}
+    )
 
     def __str__(self):
         return self.full_title
--- main/models/coin.py
+++ main/models/coin.py
@@ -1,4 +1,9 @@
 from django.db import models
+from imagekit.models import (
+    ProcessedImageField,
+    ImageSpecField,
+)
+from imagekit.processors import ResizeToFill
 from .base import CollectorsItem
 
 
@@ -6,8 +11,32 @@ class Coin(CollectorsItem):
     edition = models.IntegerField()
     weight = models.DecimalField(max_digits=4, decimal_places=2)
     diameter = models.DecimalField(max_digits=4, decimal_places=2)
-    averse_image = models.ImageField(upload_to='media/coins_images/', default='default_pic/coin-01.jpg')
-    reverse_image = models.ImageField(upload_to='media/coins_images/', default='default_pic/coin-02.jpg')
+    averse_image = ProcessedImageField(
+        upload_to='media/coins_images/',
+        default='default_pic/coin-01.jpg',
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 90}
+    )
+    reverse_image = ProcessedImageField(
+        upload_to='media/coins_images/',
+        default='default_pic/coin-02.jpg',
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 90}
+    )
+    averse_thumbnail = ImageSpecField(
+        source='averse_image',
+        processors=[ResizeToFill(150, 150)],
+        format='JPEG',
+        options={'quality': 80}
+    )
+    reverse_thumbnail = ImageSpecField(
+        source='reverse_image',
+        processors=[ResizeToFill(150, 150)],
+        format='JPEG',
+        options={'quality': 80}
+    )
 
     def __str__(self):
         return self.full_title
