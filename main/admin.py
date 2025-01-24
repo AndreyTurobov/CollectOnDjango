224d2a8cb01b4bb0c5951378f0ff504d77a87f8a
andrei_turobov <forgunn@yandex.ru>
2025-01-24 19:22:46 +0500

    connected collectors item models to the admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models.coin import Coin
+from .models.banknote import Banknote
 
-# Register your models here.
+
+@admin.register(Coin)
+class CoinAdmin(admin.ModelAdmin):
+    list_display = ('full_title', 'country', 'year', 'material', 'weight', 'diameter')
+    search_fields = ('full_title', 'country', 'year')
+
+
+@admin.register(Banknote)
+class BanknoteAdmin(admin.ModelAdmin):
+    list_display = ('full_title', 'country', 'year', 'signature', 'size', 'serial_number')
+    search_fields = ('full_title', 'country', 'year')
