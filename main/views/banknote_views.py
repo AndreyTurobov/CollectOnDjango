a87942645f9548d4b7d1a2dfcf49faf20606cb84
andrei_turobov <forgunn@yandex.ru>
2025-01-24 23:24:33 +0500

    added pagination of collectors item lists

--- main/views/banknote_views.py
+++ main/views/banknote_views.py
@@ -5,6 +5,7 @@ from ..services.banknote_service import BanknoteService
 class BanknoteListView(ListView):
     template_name = 'banknotes/list.html'
     context_object_name = 'banknotes'
+    paginate_by = 9
 
     def get_queryset(self):
-        return BanknoteService.get_all_banknotes()
+        return BanknoteService.get_all_banknotes().order_by('id')
--- main/views/coin_views.py
+++ main/views/coin_views.py
@@ -5,6 +5,7 @@ from ..services.coin_service import CoinService
 class CoinListView(ListView):
     template_name = 'coins/list.html'
     context_object_name = 'coins'
+    paginate_by = 9
 
     def get_queryset(self):
-        return CoinService.get_all_coins()
+        return CoinService.get_all_coins().order_by('id')
