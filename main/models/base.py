9759b50c180f551e84b357e6ca0ee41e8cac6620
andrei_turobov <forgunn@yandex.ru>
2025-01-24 19:40:16 +0500

    fixed error in timebased model

--- main/models/base.py
+++ main/models/base.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class TimedBaseModel(models.Model):
-    created_at = models.DateTimeField(format='%Y-%m-%d %H:%M:%S', auto_now_add=True)
-    updated_at = models.DateTimeField(format='%Y-%m-%d %H:%M:%S', auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
