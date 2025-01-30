3d18e3cd70b90da603e45715022429530a43c77d
andrei_turobov <forgunn@yandex.ru>
2025-01-30 21:05:35 +0500

    renamed src dir

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,14 @@
 """
-URL configuration for src project.
+URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to controllers. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
+Function controllers
+    1. Add an import:  from my_app import controllers
+    2. Add a URL to urlpatterns:  path('', controllers.home, name='home')
+Class-based controllers
+    1. Add an import:  from other_app.controllers import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -21,14 +21,14 @@ from django.urls import (
     path,
     include,
 )
-from main import views
+from main import controllers
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('home/', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('home/', controllers.home, name='home'),
+    path('about/', controllers.about, name='about'),
 ]
 
 if settings.DEBUG:
