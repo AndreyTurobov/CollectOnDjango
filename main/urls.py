d257b28001f97a7b70c9f7b790e4cc834034c28d
andrei_turobov <forgunn@yandex.ru>
2025-01-30 21:10:09 +0500

    fixed a small error

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 
-from main.controllers.coin_controllers.coin_list_controller import (
-    CoinListView,
-)
-from .controllers.coin_controllers.coin_detail_controller import CoinDetailController
-from .controllers.banknote_controllers.banknote_list_controller import BanknoteListController
+from main.controllers.coin_controllers.coin_detail_controller import CoinDetailController
+from main.controllers.coin_controllers.coin_list_controller import CoinListController
 from main.controllers.banknote_controllers.banknote_detail_controller import BanknoteDetailController
+from main.controllers.banknote_controllers.banknote_list_controller import BanknoteListController
 
 urlpatterns = [
-    path('coins/', CoinListView.as_view(), name='coin-list'),
+    path('coins/', CoinListController.as_view(), name='coin-list'),
     path('coins/<int:pk>/', CoinDetailController.as_view(), name='coin-detail'),
     path('banknotes/', BanknoteListController.as_view(), name='banknote-list'),
     path('banknotes/<int:pk>/', BanknoteDetailController.as_view(), name='banknote-detail'),
