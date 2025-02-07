c274cda24dc2a75c2a3cd04513896cc2afad9ec2
andrei_turobov <forgunn@yandex.ru>
2025-02-07 15:00:44 +0500

    object creation urls added

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
+from main.controllers.banknote_controllers.banknote_create_controller import (
+    BanknoteCreateController,
+)
 from main.controllers.banknote_controllers.banknote_detail_controller import (
     BanknoteDetailController,
 )
 from main.controllers.banknote_controllers.banknote_list_controller import (
     BanknoteListController,
 )
+from main.controllers.coin_controllers.coin_create_controller import CoinCreateController
 from main.controllers.coin_controllers.coin_detail_controller import (
     CoinDetailController,
 )
@@ -13,7 +17,9 @@ from main.controllers.coin_controllers.coin_list_controller import CoinListContr
 
 urlpatterns = [
     path("coins/", CoinListController.as_view(), name="coin-list"),
+    path("coins/create/", CoinCreateController.as_view(), name="create-coin"),
     path("coins/<slug:slug>/", CoinDetailController.as_view(), name="coin-detail"),
     path("banknotes/", BanknoteListController.as_view(), name="banknote-list"),
+    path("banknotes/create/", BanknoteCreateController.as_view(), name="create-banknote"),
     path("banknotes/<slug:slug>/", BanknoteDetailController.as_view(), name="banknote-detail"),
 ]
