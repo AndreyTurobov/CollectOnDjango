585339b5fd4bb7ae0dddde629f98311d195f179c
andrei_turobov <forgunn@yandex.ru>
2025-02-13 21:10:39 +0500

    refactored fake_db creation command

--- main/management/commands/fake_db.py
+++ main/management/commands/fake_db.py
@@ -1,26 +1,10 @@
-import itertools
-
 from django.core.management.base import BaseCommand
-from django.utils.text import slugify
 
 from faker import Faker
 
 from main.models.banknote_model import BanknoteModel
+from main.models.choice_models import Country, Material, State, TypeOfEdition
 from main.models.coin_model import CoinModel
-
-
-def create_unique_slug(instance, base_slug):
-    """
-    Генерирует уникальный slug на основе базового значения.
-
-    Если slug уже существует, добавляет числовой суффикс.
-    """
-    slug = base_slug
-    for i in itertools.count(1):
-        if not instance.__class__.objects.filter(slug=slug).exists():
-            break
-        slug = f"{base_slug}-{i}"
-    return slug
 
 
 class Command(BaseCommand):
@@ -38,141 +22,105 @@ class Command(BaseCommand):
         fake = Faker()
         count = options["count"]
 
-        # Заполнение модели Coin
+        # Создаем или получаем объекты для ForeignKey полей
+        countries = [
+            Country.objects.get_or_create(title=title)[0]
+            for title in [
+                "Россия",
+                "Украина",
+                "Беларусь",
+                "Казахстан",
+                "Таджикистан",
+                "Киргизия",
+                "Азербайджан",
+                "Армения",
+                "Грузия",
+                "Молдова",
+                "Туркменистан",
+                "Узбекистан",
+                "Латвия",
+                "Литва",
+                "Эстония",
+                "Польша",
+            ]
+        ]
+
+        materials = [
+            Material.objects.get_or_create(title=title)[0]
+            for title in [
+                "Серебро",
+                "Золото",
+                "Бронза",
+                "Никель",
+                "Медь",
+                "Латунь",
+                "Сталь",
+                "Алюминий",
+                "Нейзильбер",
+                "Мельхиор",
+                "Цинковый сплав",
+                "Алюминиевая бронза",
+                "Биметалл",
+                "Пластик",
+                "Бумага",
+            ]
+        ]
+
+        states = [
+            State.objects.get_or_create(title=title)[0]
+            for title in [
+                "ProofLike",
+                "BrilliantUncirculated",
+                "Uncirculated",
+                "AboutUncirculated",
+                "ExtraFine",
+                "VeryFine",
+            ]
+        ]
+
+        types_of_edition = [
+            TypeOfEdition.objects.get_or_create(title=title)[0]
+            for title in ["Commemorative", "Circulated", "Circulated/Commemorative"]
+        ]
+
         for _ in range(count):
             coin = CoinModel(
-                country=fake.random_element(
-                    elements=(
-                        "Россия",
-                        "Украина",
-                        "Беларусь",
-                        "Казахстан",
-                        "Таджикистан",
-                        "Киргизия",
-                        "Азербайджан",
-                        "Армения",
-                        "Грузия",
-                        "Молдова",
-                        "Туркменистан",
-                        "Узбекистан",
-                        "Латвия",
-                        "Литва",
-                        "Эстония",
-                        "Польша",
-                    )
-                ),
+                country=fake.random_element(elements=countries),
                 nominal=fake.random_element(elements=("1", "2", "5", "10", "20", "50", "100")),
                 currency=fake.random_element(elements=("рубль", "гривна", "тенге", "сом", "лари")),
-                year=fake.random_int(min=1900, max=2023),
+                year=str(fake.random_int(min=1900, max=2023)),
                 km_number=fake.bothify(text="KM# ??##"),
-                material=fake.random_element(
-                    elements=(
-                        "Серебро",
-                        "Золото",
-                        "Бронза",
-                        "Никель",
-                        "Медь",
-                        "Латунь",
-                        "Сталь",
-                        "Алюминий",
-                        "Нейзильбер",
-                        "Мельхиор",
-                        "Цинковый сплав",
-                        "Алюминиевая бронза",
-                        "Биметалл",
-                        "Пластик",
-                        "Бумага",
-                    )
-                ),
-                state=fake.random_element(
-                    elements=("Proof_like", "BUNC", "UNC", "AUNC", "XF", "VF")
-                ),
+                material=fake.random_element(elements=materials),
+                state=fake.random_element(elements=states),
                 in_collect=fake.boolean(),
                 description=fake.text(max_nb_chars=200),
-                type_of_edition=fake.random_element(
-                    elements=("Commemorative", "Circulated", "Circ/Comm")
-                ),
+                type_of_edition=fake.random_element(elements=types_of_edition),
                 edition=fake.random_int(min=1000, max=100000),
                 weight=fake.pydecimal(left_digits=2, right_digits=2, positive=True),
                 diameter=fake.pydecimal(left_digits=2, right_digits=2, positive=True),
+                averse_image="default_pic/coin-01.jpg",
+                reverse_image="default_pic/coin-02.jpg",
             )
-            # Формируем full_title на основе уже заполненных полей
-            coin.full_title = (
-                f"{coin.country}-{coin.nominal}-{coin.currency}-{coin.year}-{coin.description}"
-            )
-            # Генерируем уникальный slug
-            base_slug = slugify(coin.full_title)
-            coin.slug = create_unique_slug(coin, base_slug)
-            # Сохраняем объект
             coin.save()
 
-        # Заполнение модели Banknote
         for _ in range(count):
             banknote = BanknoteModel(
-                country=fake.random_element(
-                    elements=(
-                        "Россия",
-                        "Украина",
-                        "Беларусь",
-                        "Казахстан",
-                        "Таджикистан",
-                        "Киргизия",
-                        "Азербайджан",
-                        "Армения",
-                        "Грузия",
-                        "Молдова",
-                        "Туркменистан",
-                        "Узбекистан",
-                        "Латвия",
-                        "Литва",
-                        "Эстония",
-                        "Польша",
-                    )
-                ),
+                country=fake.random_element(elements=countries),
                 nominal=fake.random_element(elements=("1", "2", "5", "10", "20", "50", "100")),
                 currency=fake.random_element(elements=("рубль", "гривна", "тенге", "сом", "лари")),
-                year=fake.random_int(min=1900, max=2023),
+                year=str(fake.random_int(min=1900, max=2023)),
                 km_number=fake.bothify(text="KM# ??##"),
-                material=fake.random_element(
-                    elements=(
-                        "Серебро",
-                        "Золото",
-                        "Бронза",
-                        "Никель",
-                        "Медь",
-                        "Латунь",
-                        "Сталь",
-                        "Алюминий",
-                        "Нейзильбер",
-                        "Мельхиор",
-                        "Цинковый сплав",
-                        "Алюминиевая бронза",
-                        "Биметалл",
-                        "Пластик",
-                        "Бумага",
-                    )
-                ),
-                state=fake.random_element(
-                    elements=("Proof_like", "BUNC", "UNC", "AUNC", "XF", "VF")
-                ),
+                material=fake.random_element(elements=materials),
+                state=fake.random_element(elements=states),
                 in_collect=fake.boolean(),
                 description=fake.text(max_nb_chars=200),
-                type_of_edition=fake.random_element(
-                    elements=("Commemorative", "Circulated", "Circ/Comm")
-                ),
+                type_of_edition=fake.random_element(elements=types_of_edition),
                 signature=fake.name(),
                 size=fake.bothify(text="##x## mm"),
                 serial_number=fake.bothify(text="SN ######"),
+                averse_image="default_pic/note-01.jpg",
+                reverse_image="default_pic/note-02.jpg",
             )
-            # Формируем full_title на основе уже заполненных полей
-            banknote.full_title = (
-                f"{banknote.country}-{banknote.nominal}-{banknote.currency}-"
-                f"{banknote.year}-{banknote.description}"
-            )
-            # Генерируем уникальный slug
-            base_slug = slugify(banknote.full_title)
-            banknote.slug = create_unique_slug(banknote, base_slug)
-            # Сохраняем объект
             banknote.save()
 
         self.stdout.write(
