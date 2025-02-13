45051a8f895a5fd99130129c11223dfd89fc486c
andrei_turobov <forgunn@yandex.ru>
2025-02-13 21:06:45 +0500

    refactored base model

--- main/models/base.py
+++ main/models/base.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils.text import slugify
 
-from main.models.choices import (
-    COUNTRY_CHOICES,
-    MATERIAL_CHOICES,
-    STATE_CHOICES,
-    TYPE_OF_EDITION_CHOICES,
+from main.models.choice_models import (
+    Country,
+    Material,
+    State,
+    TypeOfEdition,
 )
 from main.models.slug_generator import generate_unique_slug
 
@@ -25,20 +25,20 @@ class TimedBaseModel(models.Model):
 class CollectorsItem(TimedBaseModel):
     """Абстрактная модель для хранения общих данных о коллекционных предметах."""
 
-    country = models.CharField(max_length=50, choices=COUNTRY_CHOICES, verbose_name="Страна")
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, verbose_name="Страна")
     nominal = models.CharField(max_length=50, verbose_name="Номинал")
     currency = models.CharField(max_length=50, verbose_name="Валюта")
     year = models.CharField(max_length=4, verbose_name="Год выпуска")
     km_number = models.CharField(max_length=50, verbose_name="KM#")
-    material = models.CharField(max_length=50, choices=MATERIAL_CHOICES, verbose_name="Материал")
-    state = models.CharField(max_length=50, choices=STATE_CHOICES, verbose_name="Состояние")
+    material = models.ForeignKey(Material, on_delete=models.PROTECT, verbose_name="Материал")
+    state = models.ForeignKey(State, on_delete=models.PROTECT, verbose_name="Состояние")
     in_collect = models.BooleanField(default=False, verbose_name="В коллекции")
     description = models.TextField(blank=True, verbose_name="Описание")
     slug = models.SlugField(
         max_length=255, allow_unicode=True, unique=True, blank=True, verbose_name="Слаг"
     )
-    type_of_edition = models.CharField(
-        max_length=50, choices=TYPE_OF_EDITION_CHOICES, verbose_name="Тип выпуска"
+    type_of_edition = models.ForeignKey(
+        TypeOfEdition, on_delete=models.PROTECT, verbose_name="Тип выпуска"
     )
 
     class Meta:
