e7f5f3a24f075a6a40c5926412adf310f3739fe5
andrei_turobov <forgunn@yandex.ru>
2025-02-13 21:08:17 +0500

    registration of new models

--- main/admin.py
+++ main/admin.py
@@ -1,38 +1,64 @@
 from django.contrib import admin
 
 from main.models.banknote_model import BanknoteModel
+from main.models.choice_models import (
+    Country,
+    Material,
+    State,
+    TypeOfEdition,
+)
 from main.models.coin_model import CoinModel
 
 
 @admin.register(CoinModel)
 class CoinAdmin(admin.ModelAdmin):
     list_display = (
-        'full_title',
-        'country',
-        'year',
-        'material',
-        'weight',
-        'diameter',
+        "full_title",
+        "country",
+        "year",
+        "material",
+        "weight",
+        "diameter",
     )
     search_fields = (
-        'full_title',
-        'country',
-        'year',
+        "full_title",
+        "country",
+        "year",
     )
 
 
 @admin.register(BanknoteModel)
 class BanknoteAdmin(admin.ModelAdmin):
     list_display = (
-        'full_title',
-        'country',
-        'year',
-        'signature',
-        'size',
-        'serial_number',
+        "full_title",
+        "country",
+        "year",
+        "signature",
+        "size",
+        "serial_number",
     )
     search_fields = (
-        'full_title',
-        'country',
-        'year',
+        "full_title",
+        "country",
+        "year",
     )
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TypeOfEdition)
+class TypeOfEditionAdmin(admin.ModelAdmin):
+    pass
