15ee6cf36b8c2d04e0562ee2022cd5bcd0db311f
andrei_turobov <forgunn@yandex.ru>
2025-02-22 15:22:11 +0500

    added docstring

--- main/models/slug_generator.py
+++ main/models/slug_generator.py
@@ -1,4 +1,5 @@
 def generate_unique_slug(model_class, base_slug, slug_field="slug"):
+    """Создает уникальный слаг объекта."""
     counter = 1
     temp_slug = base_slug
     while model_class.objects.filter(**{slug_field: temp_slug}).exists():
