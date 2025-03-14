621ec4b0377e6c464f2c892aa1dcb3804de76b7f
andrei_turobov <forgunn@yandex.ru>
2025-03-14 22:19:13 +0500

    the object forms has been changed for m2m

--- main/forms/banknote_form.py
+++ main/forms/banknote_form.py
@@ -20,7 +20,7 @@ class BanknoteForm(forms.ModelForm):
             "country": forms.Select(attrs={"class": "form-control"}),
             "material": forms.Select(attrs={"class": "form-control"}),
             "state": forms.Select(attrs={"class": "form-control"}),
-            "theme": forms.Select(attrs={"class": "form-control"}),
+            "themes": forms.SelectMultiple(attrs={"class": "form-control"}),
             "type_of_edition": forms.Select(attrs={"class": "form-control"}),
             "description": forms.Textarea(attrs={"rows": 1}),
             "placeholder": "Введите описание банкноты",
