a5e6844da03a9209df5fc78fac198b15e4a08649
andrei_turobov <forgunn@yandex.ru>
2025-03-15 15:21:25 +0500

    intermediate models have been added to the collection model

--- main/models/collection_model.py
+++ main/models/collection_model.py
@@ -5,14 +5,34 @@ from main.models.base import TimedBaseModel
 from main.models.coin_model import CoinModel
 
 
+class CollectionBanknoteModel(models.Model):
+    """Промежуточная модель для связи коллекций и банкнот."""
+
+    collection = models.ForeignKey("CollectionModel", on_delete=models.CASCADE)
+    banknote = models.ForeignKey(BanknoteModel, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+
+class CollectionCoinModel(models.Model):
+    """Промежуточная модель для связи коллекций и монет."""
+
+    collection = models.ForeignKey("CollectionModel", on_delete=models.CASCADE)
+    coin = models.ForeignKey(CoinModel, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+
 class CollectionModel(TimedBaseModel):
     title = models.CharField(max_length=100, verbose_name="Название")
     slug = models.SlugField(
         max_length=100, allow_unicode=True, unique=True, blank=True, verbose_name="Слаг"
     )
     description = models.TextField(blank=True, verbose_name="Описание")
-    banknotes = models.ManyToManyField(BanknoteModel, verbose_name="Банкноты", blank=True)
-    coins = models.ManyToManyField(CoinModel, verbose_name="Монеты", blank=True)
+    banknotes = models.ManyToManyField(
+        BanknoteModel, through="CollectionBanknoteModel", verbose_name="Банкноты", blank=True
+    )
+    coins = models.ManyToManyField(
+        CoinModel, through="CollectionCoinModel", verbose_name="Монеты", blank=True
+    )
 
     def __str__(self):
         return self.title
