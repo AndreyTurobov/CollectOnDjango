0137d0847b0a60f9a9fd6a862dcf536ee5152990
andrei_turobov <forgunn@yandex.ru>
2025-03-16 20:37:49 +0500

    added collection_dao

---
+++ main/dao/collection_dao.py
@@ -0,0 +1,22 @@
+from django.db.models import QuerySet
+
+from main.dao.base_dao import BaseDAO
+from main.models.collection_model import CollectionModel as Collection
+
+
+class CollectionDAO(BaseDAO[Collection]):
+    """DAO для работы с данными коллекций.
+
+    Наследует базовый функционал от BaseDAO и работает с моделью Collection.
+    """
+
+    def __init__(self):
+        """Инициализация DAO.
+
+        Указывает модель Collection для работы с данными.
+        """
+        super().__init__(Collection)
+
+    def get_all(self) -> QuerySet[Collection]:
+        """Возвращает все коллекции в порядке убывания даты создания."""
+        return self.model.objects.all().order_by("-created_at")
