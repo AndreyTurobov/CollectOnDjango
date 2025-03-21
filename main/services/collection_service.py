47e1e347ec2cd217fb65416b52749f504ee4e823
andrei_turobov <forgunn@yandex.ru>
2025-03-21 18:25:54 +0500

    the add_object_to_collection methods have been added to the collection service

--- main/services/collection_service.py
+++ main/services/collection_service.py
@@ -18,3 +18,15 @@ class CollectionService(BaseService):
     def get_by_slug_with_items(self, slug: str):
         """Возвращает коллекцию с предзагруженными монетами и банкнотами."""
         return self.dao.model.objects.prefetch_related("coins", "banknotes").get(slug=slug)
+
+    def add_banknote_to_collection(self, collection_slug: str, banknote_slug: str) -> None:
+        collection = self.get_by_slug(collection_slug)
+        banknote = self.get_by_slug(banknote_slug)
+        if collection and banknote:
+            self.dao.add_banknote(collection, banknote)
+
+    def add_coin_to_collection(self, collection_slug: str, coin_slug: str) -> None:
+        collection = self.get_by_slug(collection_slug)
+        coin = self.get_by_slug(coin_slug)
+        if collection and coin:
+            self.dao.add_coin(collection, coin)
